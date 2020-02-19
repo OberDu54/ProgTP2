fc639c2ddf9c9fea968a0beb4faef365e0b04e24
Ober <lucasober@hotmail.fr>
2020-02-19 22:41:09 +0100

    Ajout des classes robinet et fuite

---
+++ src/main/java/fr/ul/miage/lucas/Fuite.java
@@ -0,0 +1,25 @@
+package fr.ul.miage.lucas;
+
+public class Fuite {
+	
+	public double debit;
+
+	public Fuite(double debit) {
+		super();
+		this.debit = debit;
+	}
+	
+	public void fuir() {
+		
+	}
+	
+	public double getDebit() {
+		return debit;
+	}
+
+	public void setDebit(double debit) {
+		this.debit = debit;
+	}
+	
+	
+}
---
+++ src/main/java/fr/ul/miage/lucas/Robinet.java
@@ -0,0 +1,24 @@
+package fr.ul.miage.lucas;
+
+public class Robinet {
+	
+	private double debit;
+
+	public Robinet(double debit) {
+		super();
+		this.debit = debit;
+	}
+	
+	public void couler() {
+		
+	}
+	
+	public double getDebit() {
+		return debit;
+	}
+
+	public void setDebit(double debit) {
+		this.debit = debit;
+	}
+	
+}
