91d6b4fca9bc1f90ea7da61694ac2cee164559c2
Ober <lucasober@hotmail.fr>
2020-02-19 22:57:25 +0100

    Utilisation des options

--- src/main/java/fr/ul/miage/lucas/App.java
+++ src/main/java/fr/ul/miage/lucas/App.java
@@ -3,6 +3,13 @@ package fr.ul.miage.lucas;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,10 +20,8 @@ public class App extends Application{
 	
 	public static final Logger LOG = Logger.getLogger(App.class.getName());
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
-
+	public static Baignoire baignoire;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("TP2_Oberhausser");
@@ -30,6 +35,41 @@ public class App extends Application{
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		Options options = new Options();
+		Option cap = new Option("c", "capacity", true, "Capacite de la baignoire");
+		Option remplissage = new Option("r", "remp", true, "Debit de remplissage de la baignoire");
+		Option fuite = new Option("f", "fuite", true, "Debit de fuite");
+		cap.setRequired(false);
+		remplissage.setRequired(false);
+		fuite.setRequired(false);
+		options.addOption(cap);
+		options.addOption(remplissage);
+		options.addOption(fuite);
+		CommandLineParser parser = new DefaultParser();
+		int c = 100;	//capacite par defaut
+		double r = 2;	//remplissage par defaut
+		double f = 0.5;	//fuite par defaut
+		try {
+			CommandLine line = parser.parse(options, args);
+			if(line.hasOption('c')) {
+				c = Integer.parseInt(line.getOptionValue('c'));
+			}
+			if(line.hasOption('r')) {
+				r = Double.parseDouble(line.getOptionValue('r'));
+			}
+			if(line.hasOption('f')) {
+				f = Double.parseDouble(line.getOptionValue('f'));
+			}
+		baignoire = new Baignoire(c, new Robinet(r), new Fuite(f));	
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		LOG.info("Lancement...");
+		launch(args);
 	}
 
 }
